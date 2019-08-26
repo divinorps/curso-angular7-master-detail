6ba3c08a1d2c4f95f4060fe42edbf3e7656517b1
Divino R P Silva <divinorps@gmail.com>
2019-08-26 17:29:26 -0300

    Adicionando e atualizando categorias

--- finansys/src/app/app.module.ts
+++ finansys/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InMemoryDatabase } from './pages/in-memory-database';
-
 
 @NgModule({
   declarations: [
@@ -15,6 +16,8 @@ import { InMemoryDatabase } from './pages/in-memory-database';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   providers: [],
--- finansys/src/app/pages/categorias/categoria-form/categoria-form.component.ts
+++ finansys/src/app/pages/categorias/categoria-form/categoria-form.component.ts
@@ -1,10 +1,10 @@
 import { AfterContentChecked, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { switchMap } from 'rxjs/operators';
 import { Categoria } from '../shared/categoria.model';
 import { CategoriaService } from '../shared/categoria.service';
-
 
 @Component({
   selector: 'app-categoria-form',
@@ -23,7 +23,8 @@ export class CategoriaFormComponent implements OnInit, AfterContentChecked {
   constructor(private categoriaService: CategoriaService,
     private route: ActivatedRoute,
     private router: Router,
-    private formBuilder: FormBuilder) { }
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService) { }
 
   ngOnInit() {
     this.setCurrentAction();
@@ -33,6 +34,16 @@ export class CategoriaFormComponent implements OnInit, AfterContentChecked {
 
   ngAfterContentChecked(): void {
     this.setPageTitle();
+  }
+
+  enviarForm() {
+    this.submmitingForm = true;
+    if (this.currentAction === 'new') {
+      this.adicionarCategoria();
+    } else {
+      this.atualizarCategoria();
+    }
+
   }
 
   private setCurrentAction() {
@@ -73,6 +84,40 @@ export class CategoriaFormComponent implements OnInit, AfterContentChecked {
     } else {
       const categoriaNome = this.categoria.nome || '';
       this.pageTitulo = 'Alterando categoria: ' + categoriaNome;
+    }
+  }
+
+  private adicionarCategoria() {
+    const categoria: Categoria = Object.assign(new Categoria(), this.categoriaForm.value);
+    this.categoriaService.adiciona(categoria)
+    .subscribe(
+      (categoria) => this.actionsForSuccess(categoria)),
+      (error: any) => this.actionsForError(error);
+  }
+
+  private atualizarCategoria() {
+    const categoria: Categoria = Object.assign(new Categoria(), this.categoriaForm.value);
+    this.categoriaService.atualiza(categoria)
+    .subscribe(
+      (categoria) => this.actionsForSuccess(categoria)),
+      (error: any) => this.actionsForError(error);
+  }
+
+  private actionsForSuccess(categoria: Categoria) {
+    this.toastr.success('Solicitação processada com sucesso.');
+    this.router.navigateByUrl('categorias', { skipLocationChange: true}).then(
+      () => this.router.navigate(['categorias', categoria.id, 'edit'])
+    );
+
+  }
+
+  private actionsForError(error: any) {
+    this.toastr.error('Ocorreu um erro ao processar sua solicitação');
+    this.submmitingForm = false;
+    if (error.status === 422) {
+      this.ServerErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.ServerErrorMessages = ['Falha na comiunicação com o servidor. Tente mais novamente mais tarde!'];
     }
   }
 }
