8addf26c3dc57fb1f319e38117c73f0368cc09d7
Divino R P Silva <divinorps@gmail.com>
2019-08-26 13:35:55 -0300

    Adicionando serviço de categoria

--- finansys/src/app/pages/categorias/shared/categoria.service.ts
+++ finansys/src/app/pages/categorias/shared/categoria.service.ts
@@ -1,9 +1,69 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Categoria } from './categoria.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
 
-  constructor() { }
+  private apiPath = 'api/categorias';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Categoria[]>{
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategorias)
+    );
+  }
+
+  getPorId(id: number): Observable<Categoria> {
+    const url = `${this.apiPath}/${id}`;
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategoria)
+    );
+  }
+
+  adiciona(categoria: Categoria): Observable<Categoria> {
+    return this.http.post(this.apiPath, categoria).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategoria)
+    );
+  }
+
+  atualiza(categoria: Categoria): Observable<Categoria> {
+    const url = `${this.apiPath}/${categoria.id}`;
+    return this.http.put(url, categoria).pipe(
+      catchError(this.handleError),
+      map(() => categoria)
+    );
+  }
+
+  delete(id: number): Observable<any>{
+    const url = `${this.apiPath}/${id}`;
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
+  jsonDataToCategorias(jsonData: any[]): Categoria[] {
+    const categorias: Categoria[] = [];
+     jsonData.forEach(categoria => categorias.push(categoria as Categoria));
+      return categorias;
+  }
+
+  jsonDataToCategoria(jsonData: any): Categoria {
+    return jsonData as Categoria;
+  }
+  handleError(error: any): Observable<any> {
+    console.log('Erro na requisição => ', error);
+    return throwError(error);
+  }
+
 }
+
