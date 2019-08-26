d8ef52b278709d777bdc1c828a645a453be814d0
Divino R P Silva <divinorps@gmail.com>
2019-08-26 10:56:23 -0300

    Adicionando in-memori-database de cateegorias

---
+++ finansys/src/app/pages/in-memory-database.ts
@@ -0,0 +1,14 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb() {
+    const categorias = [
+      {id: 1, nome: 'Moradia', descricao: 'Pagamentos de contas da casa'},
+      {id: 2, nome: 'Saúde', descricao: 'Pagamentos de saúde e remédios'},
+      {id: 3, nome: 'Lazer', descricao: 'Cinama, parques, praia, etc'},
+      {id: 4, nome: 'Salário', descricao: 'Recebimento de salário'},
+      {id: 5, nome: 'Freelas', descricao: 'Trabalhos como freelancer'}
+    ];
+    return { categorias };
+  }
+}
